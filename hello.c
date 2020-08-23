e928da5b29f15e056e53b94244088bb7f3b08669
Sarah <sarah.babcock@huskers.unl.edu>
2020-08-23 15:35:22 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Sarah Babcock
+ * Date: 2020/08/20
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Sarah!\n");
+     printf("My major is computer science\n");
+
+     return 0;
+ }
